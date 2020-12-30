fd42ac7864fc67ec4e7e2db76aed58d2045434d8
heejaykong <heejay.dev@gmail.com>
2020-12-30 15:42:28 +0900

    func: add dummy phone book using RecyclerView

---
+++ app/src/main/java/com/example/madcampweek1/ExampleItem.kt
@@ -0,0 +1,3 @@
+package com.example.madcampweek1
+
+data class ExampleItem(val imageResource: Int, val text1: String, val text2: String)
--- app/src/main/java/com/example/madcampweek1/MainActivity.kt
+++ app/src/main/java/com/example/madcampweek1/MainActivity.kt
@@ -1,11 +1,33 @@
 package com.example.madcampweek1
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val exampleList = generateDummyList(500)
+
+        recycler_view.adapter = ExampleAdapter(exampleList)
+        recycler_view.layoutManager = LinearLayoutManager(this)
+        recycler_view.setHasFixedSize(true)
+    }
+    private fun generateDummyList(size: Int): List<ExampleItem> {
+        val list = ArrayList<ExampleItem>()
+
+        for (i in 0 until size) {
+            val drawable = when (i % 3) {
+                0 -> R.drawable.ic_android_head
+                1 -> R.drawable.ic_flower
+                else -> R.drawable.ic_face
+            }
+            val item = ExampleItem(drawable, "Item $i", "010-$i$i$i$i-$i$i$i$i")
+            list += item
+        }
+        return list
     }
 }
