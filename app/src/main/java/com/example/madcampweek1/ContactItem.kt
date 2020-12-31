c13436b7ce930fd9690f163faf906d3057e6d939
JeonJaeHyeong <wogud405@kaist.ac.kr>
2020-12-31 22:04:21 +0900

    func: add Phone Book load function

--- app/src/main/java/com/example/madcampweek1/ContactItem.kt
+++
@@ -1,4 +0,0 @@
-package com.example.madcampweek1
-
-data class ContactItem(val imageResource: Int, val text1: String, val text2: String)
-//data class Phone(val id:String?, val name:String?, val phone:String?)
